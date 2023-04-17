fdce343b2541f3fff93b5656dda876e309c16cc9
4TTprojet <130965849+4TTprojet@users.noreply.github.com>
2023-04-17 08:28:57 +0000

    wa

--- Index.php
+++ Index.php
@@ -15,6 +15,25 @@
     $search = $_POST['chercher'];
     
     ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#chercher').on('keydown', function(e){
+                if(e.keyCode == 13){ // Si l'utilisateur appuie sur la touche "Entrée"
+                    var searchQuery = $(this).val(); // Récupérer les termes de recherche saisis par l'utilisateur
+                    $.ajax({
+                        url: 'search.php', // L'URL du fichier PHP qui effectue la recherche
+                        method: 'POST',
+                        data: {searchQuery: searchQuery}, // Les termes de recherche saisis par l'utilisateur
+                        success: function(response){
+                            // Afficher les résultats de la recherche
+                            console.log(response);
+                        }
+                    });
+                }
+            });
+        });
+    </script>
 </span>
 </body>
 </html>
