8c0c5a915e02255aca68219eeb3a99490f932c87
4TTprojet <130965849+4TTprojet@users.noreply.github.com>
2023-04-17 07:29:04 +0000

    nico

--- Index.php
+++ Index.php
@@ -1,1 +1,28 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <select name="" id="">
+        <option value="1">1 personne</option>
+        <option value="2">2 personne</option>
+        <option value="3">3 personne</option>
+        <option value="4">4 personne</option>
+        <option value="5">5 personne</option>
+        <option value="6">6 personne</option>
+        <option value="7">7 personne</option>
+        <option value="8">8 personne</option>
+        <option value="9">9 personne</option>
+        <option value="10">10 personne</option>
+        <option value="11">11 personne</option>
+        <option value="12">12 personne</option>
+        <option value="13">13 personne</option>
+        <option value="14">14 personne</option>
+        <option value="15">15 personne</option>
+    </select>
+</body>
+</html>
